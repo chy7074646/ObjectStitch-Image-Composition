04d872de4f291359303f2a954c5341eaf16f5e26
卢阿 <chenghuiyun.chy@alibaba-inc.com>
2024-05-23 19:31:54 +0800

    Update

---
+++ downLoad_data.py
@@ -0,0 +1,14 @@
+import fiftyone
+import fiftyone.zoo
+
+if __name__ == '__main__':
+    # dataset = fiftyone.zoo.load_zoo_dataset("open-images-v7", split="validation")
+    dataset = fiftyone.zoo.load_zoo_dataset(
+        "open-images-v7",
+        split="test",
+        label_types=["detections", "segmentations", "points"],
+        classes=["Cat", "Dog"],
+        max_samples=100,
+        dataset_dir = "open-image-v7",
+    )
+    print(dataset)
